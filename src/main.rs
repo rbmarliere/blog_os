f2b7ee3c7d17c3e608cb3b2d9fc1c987328700f5
Ricardo B. Marliere <ricardo@marliere.net>
2024-06-20 10:55:25 +0000

    post-04

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,37 @@
 #![no_std]
 #![no_main]
-
-mod vga_buffer;
+#![feature(custom_test_frameworks)]
+#![test_runner(blog_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use blog_os::println;
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
+	println!("{}", info);
+	loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+	blog_os::test_panic_handler(info)
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello world{}", "!");
-    panic!("This is a panic message");
-    loop {}
+	println!("Hello world{}", "!");
+	// panic!("This is a panic message");
+
+	#[cfg(test)]
+	test_main();
+
+	loop {}
+}
+
+#[test_case]
+fn trivial_assertion() {
+	assert_eq!(1, 1);
 }
